b12c559cd3a00bd79791915612dbac94798ecae8
Niveditha <nivedithan1997@gmail.com>
2025-09-14 14:13:57 +0800

    modified sentiment code

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -5,8 +5,9 @@ sentiment_model = pipeline("sentiment-analysis")
 
 def analyze_sentiment(news_list):
     results = []
-    for headline in news_list:
-        sentiment = sentiment_model(headline)[0]
+    # analyze the sentiment of a given text (e.g., a headline) and classify it as either positive, negative, or sometimes neutral
+    sentiments = sentiment_model(news_list)
+    for headline, sentiment in zip(news_list, sentiments):
         results.append({
             "headline": headline,
             "sentiment": sentiment["label"],
